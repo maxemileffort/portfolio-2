ce603e57fe4f7f3b786141d4b13347a7572a2e01
Max Wood <max.w29@hotmail.com>
2021-03-12 12:39:12 -0600

    trying some other ideas to get contact form to work

--- form-to-email.php
+++ form-to-email.php
@@ -1,28 +1,27 @@
 <?php
-if(!isset($_POST['submit']))
-{
-    echo "error; You need to submit the contact form.";
-}
+    if(!isset($_POST['submit']))
+    {
+        echo "error: You need to submit the contact form.";
+    }
 
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-//validate
-if(empty($name)||empty($subject)||empty($message))
-{
-    echo "Entire form is mandatory.";
-    exit;
-}
+    //validate
+    if(empty($email)||empty($subject)||empty($message))
+    {
+        echo "Entire form is mandatory.";
+        exit;
+    }
 
-$email_from = 'maxemileffort@gmail.com';
-$email_subject = "topic: $subject";
-$email_body = "You have a new message from $email. \n Here is the message: \n$message";
-$to = 'maxemileffort@gmail.com';
-$headers  "From: $email_from \r\n";
+    $to = 'maxemileffort@gmail.com';
+    $email_subject = "topic: $subject";
+    $email_body = "You have a new message from $email. \n Here is the message: \n$message";
+    $headers  "From: $to \r\n";
 
-mail($to, $email_subject, $email_body, $headers);
+    mail($to, $email_subject, $email_body, $headers);
 
-header('Location: https://maxemileffort.github.io/portfolio-2#email')
+    header('Location: https://maxemileffort.github.io/portfolio-2#email')
 
 ?>
